07723557343a9a4160825b0e78e31ef38d4326d1
Toly <tolerious.feng@kuka.com>
2024-11-06 16:52:58 +0800

    🚧 Website improvement.

--- app.vue
+++ app.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
-    <!-- <NuxtRouteAnnouncer /> -->
-    <!-- <NuxtWelcome /> -->
+    <NuxtLayout>
+      <NuxtPage></NuxtPage>
+    </NuxtLayout>
   </div>
 </template>
 
 <script setup lang="ts">
+console.log("---------------------------");
 const runtimeConfig = useRuntimeConfig();
 console.log(runtimeConfig);
+
+const appConfig = useAppConfig();
+console.log(appConfig.title);
+console.log("---------------------------");
 </script>
---
+++ pages/index.vue
@@ -0,0 +1,4 @@
+<template>
+  <div>Homepage</div>
+  <div class="bg-pink-500">Content</div>
+</template>
