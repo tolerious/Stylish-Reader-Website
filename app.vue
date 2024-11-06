bbea614423beb83009ba24c98aed11805a7dc940
Toly <tolerious.feng@kuka.com>
2024-11-06 15:45:30 +0800

    🚧 Init project.

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <!-- <NuxtRouteAnnouncer /> -->
+    <!-- <NuxtWelcome /> -->
+  </div>
+</template>
+
+<script setup lang="ts">
+const runtimeConfig = useRuntimeConfig();
+console.log(runtimeConfig);
+</script>
