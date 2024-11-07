add5ef20ac4198396ddaab3593dd8e8100eca63f
Toly <tolerious.feng@kuka.com>
2024-11-07 14:00:29 +0800

    🚧 Generate robots.txt file.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/robots"],
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   runtimeConfig: { mySecret: "secret:123" },
+  robots: { allow: ["/"], disallow: ["/personalcenter"] },
 });
