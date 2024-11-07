cc48c386feedb62b5ca4eb598653e5ce88fcd1b4
Toly <tolerious.feng@kuka.com>
2024-11-07 14:26:11 +0800

    🚧 Generate sitemap.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/robots"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/robots", "@nuxtjs/sitemap"],
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   runtimeConfig: { mySecret: "secret:123" },
   robots: { allow: ["/"], disallow: ["/personalcenter"] },
+  site: { url: "https://stylishreader.com" },
 });
