d8983997bcd229f08d30cd65367439fef0ce8fdc
Toly <tolerious.feng@kuka.com>
2024-11-08 14:18:21 +0800

    🚧 Add pinia support.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/robots", "@nuxtjs/sitemap"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/robots",
+    "@nuxtjs/sitemap",
+    "@pinia/nuxt",
+  ],
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   runtimeConfig: { mySecret: "secret:123" },
